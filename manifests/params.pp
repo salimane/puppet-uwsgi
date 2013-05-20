409ce38e8d7ae2d88eb337c8b9d8e509a66d5c27
Salimane Adjao Moustapha <me@salimane.com>
2013-05-20 21:36:47 +0800

    remove obselete params since we have uwsgi::plugin now

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,10 +3,6 @@ class uwsgi::params {
         Debian,Ubuntu: {
             $package                      = 'uwsgi'
             $app_dir                      = '/etc/uwsgi'
-            $plugin_python_package        = 'uwsgi-plugin-python'
-            $plugin_rack_ruby_18_package  = 'uwsgi-plugin-rack-ruby1.8'
-            $plugin_rack_ruby_191_package = 'uwsgi-plugin-rack-ruby1.9.1'
-            $plugin_erlang_package        = 'uwsgi-plugin-erlang'
             $plugin_prefix                = 'uwsgi-plugin-'
         }
         default: {
