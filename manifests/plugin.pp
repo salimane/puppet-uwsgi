6cf2bc66e48b44d52a5bc779f69e5b83006564dd
Salimane Adjao Moustapha <me@salimane.com>
2013-05-25 19:31:55 +0800

    add ability to specify different plugin prefixes
    Conflicts:
    	manifests/params.pp

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,12 +1,19 @@
 define uwsgi::plugin (
-    $ensure = present
+    $ensure        = present,
+    $plugin_prefix = undef
 ) {
     include uwsgi
 
-    if ! defined(Package["${plugin_prefix}${name}"]) {
-        package { "${plugin_prefix}${name}":
+    if $plugin_prefix {
+        $real_plugin_prefix = $plugin_prefix
+    } else {
+        $real_plugin_prefix = $uwsgi::params::plugin_prefix
+    }
+
+    if ! defined(Package["${real_plugin_prefix}${name}"]) {
+        package { "${real_plugin_prefix}${name}":
             ensure  => $ensure,
-            notify => Service['uwsgi']
+            notify  => Class['uwsgi::service']
         }
     }
 }
